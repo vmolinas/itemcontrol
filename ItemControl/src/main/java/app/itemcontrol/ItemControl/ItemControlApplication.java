311fc7153e2a9571f2fbbf81c6e34c82af3c97c5
Victor Molinas <u.vmolinas@gmail.com>
2023-08-26 18:29:01 -0300

    Initial commit

---
+++ ItemControl/src/main/java/app/itemcontrol/ItemControl/ItemControlApplication.java
@@ -0,0 +1,13 @@
+package app.itemcontrol.ItemControl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItemControlApplication.class, args);
+	}
+
+}
---
+++ ItemControl/src/test/java/app/itemcontrol/ItemControl/ItemControlApplicationTests.java
@@ -0,0 +1,13 @@
+package app.itemcontrol.ItemControl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
